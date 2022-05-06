af8a9a1c7d192ca2ad68b5512db1deaf6173c6c9
b21leowa <b21leowa@student.his.se>
2022-05-06 17:26:55 +0200

    The second activity mainButton now closes the intent and returns to main activity

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,16 +1,31 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class SecondActivity extends AppCompatActivity {
 
+    Button mainAction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        mainAction = findViewById(R.id.mainAction);
+
+        mainAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
+
+
 
 }

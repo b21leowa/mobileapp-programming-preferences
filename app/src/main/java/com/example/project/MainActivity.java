804d54bddfbd10b2f4b1911ecab3550d30f91711
b21leowa <b21leowa@student.his.se>
2022-05-06 17:37:11 +0200

    You can now edit the text but it only changes when you restart the software

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,7 +13,10 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     Button editTextButton;
-
+    TextView titleEdit;
+    String value;
+    private SharedPreferences prefs;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +26,15 @@ public class MainActivity extends AppCompatActivity {
 
 
         editTextButton = findViewById(R.id.changeTitle);
+        titleEdit = findViewById(R.id.editThisText);
+
+        prefs = getSharedPreferences("test", MODE_PRIVATE);
+        titleEdit.setText(prefs.getString("value", "Change this Text!"));
 
         editTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,8 +1,10 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,17 +12,26 @@ import androidx.appcompat.widget.Toolbar;
 public class SecondActivity extends AppCompatActivity {
 
     Button mainAction;
-
+    EditText editText;
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editPrefs;
+    String value = "Change this text";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
         mainAction = findViewById(R.id.mainAction);
+        editText = findViewById(R.id.editText);
 
         mainAction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                value = editText.getText().toString().trim();
+                prefs = getSharedPreferences("test", MODE_PRIVATE);
+                editPrefs = prefs.edit();
+                editPrefs.putString("value", value);
+                editPrefs.apply();
                 finish();
             }
         });
